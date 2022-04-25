253867dfdcf30dff9dbb0933f81789721ddef366
LWY <amethyst270@naver.com>
2022-04-25 15:40:46 +0900

    feat: kotlin skeleton code 수정

--- Frontend/Kotlin/keep_my_receipt/app/src/main/java/com/ssafy/keep_my_receipt/src/main/MainActivity.kt
+++ Frontend/Kotlin/keep_my_receipt/app/src/main/java/com/ssafy/keep_my_receipt/src/main/MainActivity.kt
@@ -1,19 +1,9 @@
 package com.ssafy.keep_my_receipt.src.main
-
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.text.Spannable
-import android.text.SpannableStringBuilder
-import android.text.style.ForegroundColorSpan
-import android.util.Log
-import android.widget.TextView
 import com.ssafy.keep_my_receipt.R
 import com.ssafy.keep_my_receipt.config.BaseActivity
 import com.ssafy.keep_my_receipt.databinding.ActivityMainBinding
-import com.ssafy.keep_my_receipt.util.ProgressDialog
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-    private lateinit var customProgressDialog: ProgressDialog
 
     override fun onStart() {
         super.onStart()
@@ -21,17 +11,5 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     }
 
     private fun initStatus() {
-        // 로딩창
-        customProgressDialog = ProgressDialog(this)
-        customProgressDialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
     }
-
-//    fun setColorInPartitial(pre_string:String, string:String, color:String, textView:TextView) : TextView
-//    {
-//        val builder:SpannableStringBuilder = SpannableStringBuilder(pre_string + string)
-//        builder.setSpan(ForegroundColorSpan (Color.parseColor(color)), 0, pre_string.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-//        textView.append(builder)
-//        return textView
-//    }
 }
--- Frontend/Kotlin/keep_my_receipt/app/src/main/java/com/ssafy/keep_my_receipt/src/splash/SplashActivity.kt
+++ Frontend/Kotlin/keep_my_receipt/app/src/main/java/com/ssafy/keep_my_receipt/src/splash/SplashActivity.kt
@@ -1,11 +1,13 @@
 package com.ssafy.keep_my_receipt.src.splash
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import com.ssafy.keep_my_receipt.R
 import com.ssafy.keep_my_receipt.config.BaseActivity
 import com.ssafy.keep_my_receipt.databinding.ActivitySplashBinding
+import com.ssafy.keep_my_receipt.src.main.MainActivity
 
 class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
     private val DELAY = 1000 // 원하는 값으로 설정
@@ -14,7 +16,7 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_spl
         super.onCreate(savedInstanceState)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            //startActivity(Intent(this, LoginActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }, DELAY.toLong())
     }
