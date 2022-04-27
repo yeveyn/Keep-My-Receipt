b5452db1459946cc635706bc87971bb772c8b541
DESKTOP-C70T359\SEO <seojiwon335@gmail.com>
2022-04-27 16:46:51 +0900

    S06P31D104-259 / feat : add tesseract skeleton code

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -1,13 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from tesseract import tesseractOCR
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/fast/ocr/receipt")
+async def analysisReceipt(file : UploadFile = File(...)):
+    text = tesseractOCR(file)
+    return {"message": text}
