d5d583d7fa1f776750ffff3992f5eedb69e481bf
Inho <adiens916@naver.com>
2022-04-28 20:49:21 +0900

    S06P31D104-184 / feat: 거래 내역 리스트 뷰 구현

--- Frontend/React/keep_my_receipt/src/domain/Book/Index/index.tsx
+++ Frontend/React/keep_my_receipt/src/domain/Book/Index/index.tsx
@@ -2,6 +2,7 @@ import { Container } from '@mui/material';
 
 import useToggle from '../../../hooks/useToggle';
 import Header from './Header';
+import List from './List';
 
 export default function BookIndex() {
   const { toggleValue, ToggleButtons } = useToggle(['목록', '달력']);
@@ -13,7 +14,7 @@ export default function BookIndex() {
       {/* 토글 버튼들 */}
       <ToggleButtons />
       {/* 토글 버튼에 따라 하단 모드 변경 */}
-      {toggleValue === '목록' ? '이번 달 내용' : ''}
+      {toggleValue === '목록' ? <List /> : ''}
     </Container>
   );
 }
