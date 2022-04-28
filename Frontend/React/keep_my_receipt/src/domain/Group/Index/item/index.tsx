3fe70e1ba1b1c81323cd0ada80fd1f180753a883
ssy0061 <dbr00061@naver.com>
2022-04-29 01:27:43 +0900

    S06P31D104-188 / feat: 모임 목록(내 모임)

--- Frontend/React/keep_my_receipt/src/domain/Group/Index/item/index.tsx
+++ Frontend/React/keep_my_receipt/src/domain/Group/Index/item/index.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
-
-export default function IndexItem() {
-  return <div>IndexItem</div>;
+import LocalDiningIcon from '@mui/icons-material/LocalDining';
+import { CardActions, CardContent, Button, Card, Grid } from '@mui/material';
+export default function IndexItem({
+  name,
+  budget,
+}: {
+  name: string;
+  budget: number;
+}) {
+  return (
+    <Grid item xs={12}>
+      <Card
+        variant="outlined"
+        sx={{
+          width: 300,
+          ':hover': {
+            boxShadow: 6,
+          },
+        }}
+      >
+        <CardContent>
+          <LocalDiningIcon fontSize="large" />
+          <div>{name}</div>
+          <div>남은 예산: {budget}</div>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  );
 }
