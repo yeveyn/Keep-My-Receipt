c7ed586dad5a92ff09c06005414cfc4c5b8cce1c
Inho <adiens916@naver.com>
2022-04-28 15:02:52 +0900

    S06P31D104-260 / fix: index.tsx 빈 파일 오류 수정

--- Frontend/React/keep_my_receipt/src/domain/Analytics/LargeTagChart/index.tsx
+++ Frontend/React/keep_my_receipt/src/domain/Analytics/LargeTagChart/index.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function LargeTagChart() {
   return <div>LargeTagChart</div>;
 }
---
+++ Frontend/React/keep_my_receipt/src/domain/Book/List/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { IconButton } from '@mui/material';
+import { ArrowLeft } from '@mui/icons-material';
+
+export default function TransactionRead() {
+  return (
+    <>
+      <IconButton size="large">
+        <ArrowLeft />
+      </IconButton>
+    </>
+  );
+}
