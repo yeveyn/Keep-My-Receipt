bd779d369bbc8ca1850272ddbfb43746808ea47d
양아름 <yeveyn@gmail.com>
2022-04-30 04:14:16 +0900

    S06P31D104-213 / fix: WebConfig 추가

--- Backend/Spring/src/main/java/com/ieung/receipt/config/WebConfig.java
+++ Backend/Spring/src/main/java/com/ieung/receipt/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://k6d104.p.ssafy.io:8185")
+                .allowedOrigins("http://k6d104.p.ssafy.io")
                 .allowedOrigins("http://k6d104.p.ssafy.io:3000");
     }
 }
