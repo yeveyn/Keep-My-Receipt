cf2cd197c902986e98a476fddac0e4f53e7ac610
양아름 <yeveyn@gmail.com>
2022-04-30 04:12:46 +0900

    S06P31D104-213 / fix: WebConfig 추가

--- Backend/Spring/src/main/java/com/ieung/receipt/config/SwaggerConfig.java
+++ Backend/Spring/src/main/java/com/ieung/receipt/config/SwaggerConfig.java
@@ -28,7 +28,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
-                .addServersItem(new Server().url("https://localhost"))
+                .addServersItem(new Server().url("https://k6d104.p.ssafy.io"))
                 .info(new Info().title("Keep My Receipt API")
                         .description("Keep My Receipt 명세서입니다.")
                         .version("V1"));
---
+++ Backend/Spring/src/main/java/com/ieung/receipt/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.ieung.receipt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://k6d104.p.ssafy.io:8185")
+                .allowedOrigins("http://k6d104.p.ssafy.io:3000");
+    }
+}
