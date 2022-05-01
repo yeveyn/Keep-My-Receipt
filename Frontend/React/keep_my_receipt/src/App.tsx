0453ec1711efbd8227649cd9500234bedd450fb6
Inho <adiens916@naver.com>
2022-05-01 23:04:33 +0900

    S06P31D104-179 / feat: 거래 내역 입력 화면 일부 구현
    - 토글에 따라 대분류 & 중분류 바뀜

--- Frontend/React/keep_my_receipt/src/App.tsx
+++ Frontend/React/keep_my_receipt/src/App.tsx
@@ -3,6 +3,7 @@ import GroupIndex from './domain/Group/Index';
 import GroupCreate from './domain/Group/Create';
 import GroupSearch from './domain/Group/Search';
 import BookIndex from './domain/Book/Index';
+import BookCreate from './domain/Book/Create';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/group/create" element={<GroupCreate />} />
         <Route path="/group/search" element={<GroupSearch />} />
         <Route path="/book/index" element={<BookIndex />} />
+        <Route path="/book/create" element={<BookCreate />} />
       </Routes>
     </BrowserRouter>
   );
