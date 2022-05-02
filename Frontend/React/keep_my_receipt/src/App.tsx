632fb6c4b6f5c2eaa142bb4b87a815c289aa9ff5
ssy0061 <dbr00061@naver.com>
2022-05-02 12:03:15 +0900

    S06P31D104-267 / feat: route 구조 변경

--- Frontend/React/keep_my_receipt/src/App.tsx
+++ Frontend/React/keep_my_receipt/src/App.tsx
@@ -1,18 +1,50 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import GroupIndex from './domain/Group/Index';
-import GroupCreate from './domain/Group/Create';
-import GroupSearch from './domain/Group/Search';
+import { BrowserRouter, Routes, Route, Outlet, Link } from 'react-router-dom';
+import ClubIndex from './domain/Club/Index';
+import ClubCreate from './domain/Club/Create';
+import ClubSearch from './domain/Club/Search';
 import BookIndex from './domain/Book/Index';
 import BookCreate from './domain/Book/Create';
+import { Container } from '@mui/material';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* 밑에 Route들 추가하시면 됩니다! */}
-        <Route path="/group/index" element={<GroupIndex />} />
-        <Route path="/group/create" element={<GroupCreate />} />
-        <Route path="/group/search" element={<GroupSearch />} />
+        <Route
+          path="/"
+          element={
+            <Container maxWidth="md">
+              <p>전체 페이지 nav bar</p>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/club">Club</Link>
+                </li>
+                <li>
+                  <Link to="/book">Book</Link>
+                </li>
+              </ul>
+              <Outlet />
+            </Container>
+          }
+        >
+          <Route index element={<p>랜딩 페이지</p>} />
+          <Route path="club" element={<Outlet />}>
+            <Route index element={<ClubIndex />} />
+            <Route path="create" element={<ClubCreate />} />
+            <Route path="search" element={<ClubSearch />} />
+          </Route>
+          <Route path="book" element={<Outlet />}>
+            <Route index element={<BookIndex />} />
+          </Route>
+        </Route>
+
+        {/* <Route path="/club/index" element={<ClubIndex />} />
+        <Route path="/club/create" element={<ClubCreate />} />
+        <Route path="/club/search" element={<ClubSearch />} /> */}
         <Route path="/book/index" element={<BookIndex />} />
         <Route path="/book/create" element={<BookCreate />} />
       </Routes>
