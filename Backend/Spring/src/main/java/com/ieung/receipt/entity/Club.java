e3aa31d9d8a74b79189d5d843e9d520ca719ff77
양아름 <yeveyn@gmail.com>
2022-05-02 17:58:14 +0900

    S06P31D104-208 / feat: 모임 CRUD API 구현

---
+++ Backend/Spring/src/main/java/com/ieung/receipt/entity/Club.java
@@ -0,0 +1,48 @@
+package com.ieung.receipt.entity;
+
+import com.ieung.receipt.dto.res.ClubResDTO;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "club")
+// 모임 테이블
+public class Club {
+    // 모임 고유 키 값
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "club_id")
+    private long id;
+
+    // 모임 이름
+    @Column(nullable = false, length = 64)
+    private String name;
+
+    // 모임 소개
+    @Column(length = 255)
+    private String description;
+
+    public void updateName(String name) {
+        this.name = name;
+    }
+
+    public void updateDescription(String description) {
+        this.description = description;
+    }
+
+    // Club을 ClubResDTO 객체로 변환
+    public ClubResDTO toClubResDTO() {
+        return ClubResDTO.builder()
+                .id(id)
+                .name(name)
+                .description(description)
+                .build();
+    }
+}
---
+++ Backend/Spring/src/main/java/com/ieung/receipt/service/ClubService.java
@@ -0,0 +1,118 @@
+package com.ieung.receipt.service;
+
+import com.ieung.receipt.code.AuthCode;
+import com.ieung.receipt.code.StateCode;
+import com.ieung.receipt.dto.req.ClubReqDTO;
+import com.ieung.receipt.entity.Crew;
+import com.ieung.receipt.entity.Club;
+import com.ieung.receipt.entity.ClubCrew;
+import com.ieung.receipt.exception.ApiMessageException;
+import com.ieung.receipt.repository.ClubCrewRepository;
+import com.ieung.receipt.repository.ClubRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ClubService {
+    private final ClubRepository clubRepository;
+    private final ClubCrewRepository clubCrewRepository;
+
+    /**
+     * 모임 생성
+     * @param crew, groupReqDTO
+     */
+    @Transactional(readOnly = false)
+    public void createClub(Crew crew, ClubReqDTO groupReqDTO) {
+        // DB에 저장할 Group Entity 세팅
+        Club group = Club.builder()
+                .name(groupReqDTO.getName())
+                .description(groupReqDTO.getDescription())
+                .build();
+
+        Club resGroup = clubRepository.save(group);
+        if (resGroup == null) {
+            throw new ApiMessageException("그룹 생성에 실패했습니다. 다시 시도해 주세요.");
+        }
+
+        // DB에 저장할 Crew (Leader) Entity 세팅
+        ClubCrew clubCrew = ClubCrew.builder()
+                .club(resGroup)
+                .crew(crew)
+                .auth(AuthCode.L) // 리더
+                .state(StateCode.A) // 승인 완료
+                .build();
+
+        ClubCrew resClubCrew = clubCrewRepository.save(clubCrew);
+        if (resClubCrew == null) {
+            throw new ApiMessageException("그룹 생성에 실패했습니다. 다시 시도해 주세요.");
+        }
+    }
+
+    /**
+     * 특정 모임 조회
+     * @param clubId
+     */
+    public Club getClub(Long clubId) {
+        Club club = clubRepository.findById(clubId).orElseThrow(() -> new ApiMessageException("존재하지 않는 모임입니다."));
+
+        return club;
+    }
+
+    /**
+     * 모임 목록 조회
+     * @param pageable
+     */
+    public Page<Club> getClubs(Pageable pageable) {
+        return clubRepository.findAll(pageable);
+    }
+
+    /**
+     * 모임 삭제
+     * @param crewId, clubId
+     */
+    @Transactional(readOnly = false)
+    public void deleteClub(Long crewId, Long clubId) {
+        ClubCrew clubCrew = clubCrewRepository.findByCrewIdAndClubId(crewId, clubId)
+                                                 .orElseThrow(() -> new ApiMessageException("가입된 모임이 아닙니다."));
+
+        if (clubCrew.getAuth() == AuthCode.L) {
+            Long clubCrewCnt = clubCrewRepository.findApprovedCountByClubId(clubId);
+
+            if (clubCrewCnt > 1) {
+                new ApiMessageException("회원 수가 1일때만 삭제할 수 있습니다.");
+            } else {
+                clubCrewRepository.delete(clubCrew);
+                clubRepository.delete(clubCrew.getClub());
+            }
+        } else {
+            throw new ApiMessageException("리더만 삭제할 수 있습니다.");
+        }
+    }
+
+    /**
+     * 모임 정보 수정
+     * @param crewId, clubId, clubReqDTO
+     */
+    @Transactional(readOnly = false)
+    public void updateClub(Long crewId, Long clubId, ClubReqDTO clubReqDTO) {
+        ClubCrew clubCrew = clubCrewRepository.findByCrewIdAndClubId(crewId, clubId)
+                                                 .orElseThrow(() -> new ApiMessageException("가입된 모임이 아닙니다."));
+
+        if (clubCrew.getAuth() == AuthCode.L) {
+            Club club = clubCrew.getClub();
+            club.updateName(clubReqDTO.getName());
+            club.updateDescription(clubReqDTO.getDescription());
+            clubRepository.save(club);
+        } else {
+            throw new ApiMessageException("리더만 수정할 수 있습니다.");
+        }
+    }
+}
