dd8301106daaf31da093f765af9114702424627d
LWY <amethyst270@naver.com>
2022-05-02 22:19:11 +0900

    feat: 설정 레이아웃

--- Frontend/React/keep_my_receipt/src/domain/Book/Index/ListItem/index.tsx
+++ Frontend/React/keep_my_receipt/src/domain/Book/Index/ListItem/index.tsx
@@ -22,6 +22,7 @@ export default function ListItem({ items }: { items: ItemType[] }) {
             {/* 거래내역 태그? */}
             <Typography variant="subtitle2">결제</Typography>
           </Stack>
+
           {/* 거래내역 금액 */}
           <Typography>{item.amount}</Typography>
         </Stack>
