14ba77c36d4f5631495b11d3cc6d9c6f77d88bab
Inho <adiens916@naver.com>
2022-05-04 23:05:48 +0900

    S06P31D104-317 / feat: 중분류 CRUD 기능

--- Frontend/React/keep_my_receipt/src/domain/Book/Create/Item/index.tsx
+++ Frontend/React/keep_my_receipt/src/domain/Book/Create/Item/index.tsx
@@ -2,14 +2,21 @@ import { useEffect, useState } from 'react';
 import { Divider, List, ListItem, Stack } from '@mui/material';
 
 import useToggle from '../../../../hooks/useToggle';
+import ItemCategoryEditable from '../ItemCategoryEditable';
 import ItemCategoryFixed from '../ItemCategoryFixed';
 import ItemInfoOnEdit from '../ItemInfoOnEdit';
 import ItemInfoOnShow from '../ItemInfoOnShow';
 import ListItemTextWithSubtext from '../ListItemTextWithSubtext';
 
+export interface ItemInfoType {
+  name: string;
+  onEdit: boolean;
+  price: number;
+}
+
 // string 배열로 구성된 객체 타입 일반화
 // 자세한 건 TypeScript의 인덱스 시그니처 참고
-interface StringArrayObjectType {
+export interface StringArrayObjectType {
   [key: string]: string[];
 }
 
@@ -25,7 +32,7 @@ const largeCategories = {
   수입: ['상금수익', '부스수익', '이자수익'],
 } as StringArrayObjectType;
 
-const mediumCategories = {
+const sampleMediumCategories = {
   '': [''],
   '현금 및 현금성자산': ['현금'],
   유형자산: ['비품', '차량'],
@@ -46,18 +53,15 @@ const mediumCategories = {
   이자수익: ['이자수익'],
 } as StringArrayObjectType;
 
-export interface ItemInfoType {
-  name: string;
-  onEdit: boolean;
-  price: number;
-}
-
 export default function Item() {
   // 토글 값과, 토글 버튼 생성
   const { toggleValue, ToggleButtons } = useToggle(classification);
   // 대분류와 중분류 값 관리
-  const [largeCategory, setLargeCategory] = useState('');
-  const [mediumCategory, setMediumCategory] = useState('');
+  const [selectedLargeCategory, setSelectedLargeCategory] = useState('');
+  const [totalMediums, setTotalMediums] = useState(sampleMediumCategories);
+  const [currentMediumCategories, setCurrentMediumCategories] = useState(['']);
+
+  const [selectedMediumCategory, setSelectedMediumCategory] = useState('');
   const [itemInfo, setItemInfo] = useState({
     name: '축구공',
     onEdit: false,
@@ -66,14 +70,25 @@ export default function Item() {
 
   // 토글 값이 바뀔 때 대분류와 중분류 초기화
   useEffect(() => {
-    setLargeCategory('');
-    setMediumCategory('');
+    setSelectedLargeCategory('');
+    setSelectedMediumCategory('');
   }, [toggleValue]);
 
   // 대분류가 바뀔 때, 중분류 초기화
   useEffect(() => {
-    setMediumCategory('');
-  }, [largeCategory]);
+    setSelectedMediumCategory('');
+    setCurrentMediumCategories(totalMediums[selectedLargeCategory]);
+  }, [selectedLargeCategory]);
+
+  // 중분류 수정/삭제 시, 기존 중분류 갱신
+  useEffect(() => {
+    console.log('현재 대분류', selectedLargeCategory);
+    console.log('현재 중분류 리스트', currentMediumCategories);
+    setTotalMediums((obj) => ({
+      ...obj,
+      [selectedLargeCategory]: currentMediumCategories,
+    }));
+  }, [currentMediumCategories]);
 
   return (
     <>
@@ -91,16 +106,17 @@ export default function Item() {
       <ItemCategoryFixed
         name="대분류"
         list={largeCategories[toggleValue]}
-        category={largeCategory}
-        setCategory={setLargeCategory}
+        category={selectedLargeCategory}
+        setCategory={setSelectedLargeCategory}
       />
       <Divider />
 
-      <ItemCategoryFixed
+      <ItemCategoryEditable
         name="중분류"
-        list={mediumCategories[largeCategory]}
-        category={mediumCategory}
-        setCategory={setMediumCategory}
+        list={currentMediumCategories}
+        setList={setCurrentMediumCategories}
+        category={selectedMediumCategory}
+        setCategory={setSelectedMediumCategory}
       />
       <Divider />
 
