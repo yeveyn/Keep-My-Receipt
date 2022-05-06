25f2a71d2513d22326e17bb367d00936b2bc8d42
LWY <amethyst270@naver.com>
2022-05-06 16:07:39 +0900

    fixed: auth-context.js file delete

--- Frontend/React/keep_my_receipt/src/store/auth-context.js
+++ Frontend/React/keep_my_receipt/src/store/auth-context.js
@@ -1,40 +1,40 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-// login 이후 accessToken 저장소
-const AuthContext = React.createContext({
-  token: '',
-  isLoggedIn: false,
-  login: (token) => {},
-  logout: () => {},
-});
+// // login 이후 accessToken 저장소
+// const AuthContext = React.createContext({
+//   token: '',
+//   isLoggedIn: false,
+//   login: (token) => {},
+//   logout: () => {},
+// });
 
-//인증 관리 역할
-export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+// //인증 관리 역할
+// export const AuthContextProvider = (props) => {
+//   const [token, setToken] = useState(null);
 
-  // 토큰이 잇으면 true, 토큰없으면 false 반환
-  const userIsLoggedIn = !!token;
+//   // 토큰이 잇으면 true, 토큰없으면 false 반환
+//   const userIsLoggedIn = !!token;
 
-  // 토큰 상태 바꾸는 함수
-  const loginHandler = () => {
-    setToken(token);
-  };
-  const logoutHandler = () => {
-    setToken(null);
-  };
+//   // 토큰 상태 바꾸는 함수
+//   const loginHandler = () => {
+//     setToken(token);
+//   };
+//   const logoutHandler = () => {
+//     setToken(null);
+//   };
 
-  const contextValue = {
-    token: token,
-    isLoggedIn: userIsLoggedIn,
-    login: loginHandler,
-    logout: logoutHandler,
-  };
+//   const contextValue = {
+//     token: token,
+//     isLoggedIn: userIsLoggedIn,
+//     login: loginHandler,
+//     logout: logoutHandler,
+//   };
 
-  return (
-    <AuthContext.Provider value={contextValue}>
-      {props.children}
-    </AuthContext.Provider>
-  );
-};
+//   return (
+//     <AuthContext.Provider value={contextValue}>
+//       {props.children}
+//     </AuthContext.Provider>
+//   );
+// };
 
-export default AuthContext;
+// export default AuthContext;
