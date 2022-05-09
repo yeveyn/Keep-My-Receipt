b9aad04b5254d859b8557503ed9f2efd207024b8
ssy0061 <dbr00061@naver.com>
2022-05-09 11:07:32 +0900

    feat: position fixed

--- Frontend/React/keep_my_receipt/src/footer/index.tsx
+++ Frontend/React/keep_my_receipt/src/footer/index.tsx
@@ -25,6 +25,7 @@ export default function SimpleBottomNavigation() {
         bottom: 0,
         left: 0,
         right: 0,
+        padding: 0,
       }}
     >
       <BottomNavigation showLabels>
