d503d5eb138b872306ce4bbbf127520079932826
Inho <adiens916@naver.com>
2022-05-09 14:23:43 +0900

    S06P31D104-325 / refactor: 샘플 태그 목록 분리하기

--- Frontend/React/keep_my_receipt/src/domain/Book/Create/Item/index.tsx
+++ Frontend/React/keep_my_receipt/src/domain/Book/Create/Item/index.tsx
@@ -1,50 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { Divider, List, Stack } from '@mui/material';
 
-import useToggle from '../../../../hooks/useToggle';
-import useEditableItem from '../../../../hooks/useEditableItem';
+// 컴포넌트
 import ItemCategoryEditable from '../ItemCategoryEditable';
 import ItemCategoryFixed from '../ItemCategoryFixed';
+// 훅
+import useToggle from '../../../../hooks/useToggle';
+import useEditableItem from '../../../../hooks/useEditableItem';
+// 리듀서
 import { BookAction, BookItemType, updateItem } from '../../bookReducer';
-
-// string 배열로 구성된 객체 타입 일반화
-// 자세한 건 TypeScript의 인덱스 시그니처 참고
-export interface StringArrayObjectType {
-  [key: string]: string[];
-}
-
-const classification = ['자산', '지출', '수입', '예산'];
-
-// as type을 씀으로써
-// 리터럴 타입만 쓰게 하는 걸 방지
-// 자세한 건 TypeScript의 리터럴 타입과 타입 단언 참고
-const largeCategories = {
-  자산: ['현금 및 현금성자산', '유형자산', '선급금', '기타자산'],
-  예산: ['전기예산', '활동지원금', '회비'],
-  지출: ['복리후생비', '여가교통비', '소모품비', '기타비용'],
-  수입: ['상금수익', '부스수익', '이자수익'],
-} as StringArrayObjectType;
-
-const sampleMediumCategories = {
-  '': [''],
-  '현금 및 현금성자산': ['현금'],
-  유형자산: ['비품', '차량'],
-  선급금: ['회원권'],
-  기타자산: ['미분류자산'],
-
-  전기예산: ['전기예산'],
-  활동지원금: ['활동지원금'],
-  회비: ['회비'],
-
-  복리후생비: ['회식', '식비', 'MT'],
-  여가교통비: ['교통비'],
-  소모품비: ['사무용품', '생활용품'],
-  기타비용: ['미분류비용'],
-
-  상금수익: ['상금수익'],
-  부스수익: ['부스수익'],
-  이자수익: ['이자수익'],
-} as StringArrayObjectType;
+// 샘플 파일들
+import {
+  mainCategories,
+  largeCategories,
+  mediumCategories,
+} from '../../tagListSample';
 
 interface ItemType {
   item: BookItemType;
@@ -54,10 +24,10 @@ interface ItemType {
 
 export default function Item({ item, itemIndex, dispatch }: ItemType) {
   // 토글 값과, 토글 버튼 생성
-  const { toggleValue, ToggleButtons } = useToggle(classification);
+  const { toggleValue, ToggleButtons } = useToggle(mainCategories);
   // 대분류와 중분류 값 관리
   const [selectedLargeCategory, setSelectedLargeCategory] = useState('');
-  const [totalMediums, setTotalMediums] = useState(sampleMediumCategories);
+  const [totalMediums, setTotalMediums] = useState(mediumCategories);
   const [currentMediumCategories, setCurrentMediumCategories] = useState(['']);
 
   const [selectedMediumCategory, setSelectedMediumCategory] = useState('');
@@ -94,8 +64,6 @@ export default function Item({ item, itemIndex, dispatch }: ItemType) {
 
   // 중분류 수정/삭제 시, 기존 중분류 갱신
   useEffect(() => {
-    console.log('현재 대분류', selectedLargeCategory);
-    console.log('현재 중분류 리스트', currentMediumCategories);
     setTotalMediums((obj) => ({
       ...obj,
       [selectedLargeCategory]: currentMediumCategories,
