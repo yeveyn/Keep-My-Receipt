8f518bce3b01c3e14ae7de3da5a74af7ebc98940
양아름 <yeveyn@gmail.com>
2022-05-11 16:49:29 +0900

    S06P31D104-280 / fix: 회원 리스트 수정

--- Backend/Spring/src/main/java/com/ieung/receipt/controller/TransactionController.java
+++ Backend/Spring/src/main/java/com/ieung/receipt/controller/TransactionController.java
@@ -1,6 +1,7 @@
 package com.ieung.receipt.controller;
 
 import com.ieung.receipt.dto.req.TransactionReqDTO;
+import com.ieung.receipt.entity.Transaction;
 import com.ieung.receipt.service.TransactionService;
 import com.ieung.receipt.service.common.CommonResult;
 import com.ieung.receipt.service.common.ResponseService;
@@ -27,14 +28,14 @@ public class TransactionController {
     private final TransactionService transactionService;
     private final ResponseService responseService;
 
-    // 거래내역 등록
-    @Operation(summary = "거래내역 등록", description = "거래내역 등록")
-    @PostMapping(value = "/{clubId}/transaction", produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody CommonResult createTransaction(@PathVariable @NotNull Long clubId, @Valid @RequestBody TransactionReqDTO transactionReqDTO) throws Exception {
-         transactionService.createTransaction(clubId, getCurrentCrewId(), transactionReqDTO);
-
-         // 알림 로직 추가
-
-        return responseService.getSuccessResult();
-    }
+//    // 거래내역 등록
+//    @Operation(summary = "거래내역 등록", description = "거래내역 등록")
+//    @PostMapping(value = "/{clubId}/transaction", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public @ResponseBody CommonResult createTransaction(@PathVariable @NotNull Long clubId, @Valid @RequestBody TransactionReqDTO transactionReqDTO) throws Exception {
+//         Transaction transaction = transactionService.createTransaction(clubId, getCurrentCrewId(), transactionReqDTO);
+//
+//         // 알림 로직 추가
+//
+//        return responseService.getSuccessResult();
+//    }
 }
