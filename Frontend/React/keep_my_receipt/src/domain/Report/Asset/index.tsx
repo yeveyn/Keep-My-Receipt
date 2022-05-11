387b25264b46d79fc13332da5136aaaf8917cb70
Inho <adiens916@naver.com>
2022-05-11 21:44:46 +0900

    S06P31D104-292 / feat: 기간설정 틀 추가 & 타입 분리하기

--- Frontend/React/keep_my_receipt/src/domain/Report/Asset/index.tsx
+++ Frontend/React/keep_my_receipt/src/domain/Report/Asset/index.tsx
@@ -1,39 +1,68 @@
+import { useState } from 'react';
+import { Button, Card, Grid, Typography } from '@mui/material';
+import { AssetReportType } from './type';
 import sample from './sample.json';
-
-type MainCategoryValueType = {
-  total: number;
-  categories: {
-    name: string;
-    sum: number;
-  }[];
-};
-
-// 배열로부터 리터럴 타입 동적으로 생성하기
-// : as const로 readonly로 만든 다음,
-// typeof로 타입을 추출해낸다.
-// 참고: https://stackoverflow.com/questions/51521808/how-do-i-create-a-type-from-a-string-array-in-typescript
-const mainCategories = [
-  '현금 및 현금성 자산',
-  '유형자산',
-  '선급금',
-  '기타자산',
-] as const;
-type MainCategoryKey = typeof mainCategories[number];
-
-type AssetReportType = {
-  // 인덱스 시그니처의 키는 매핑된 개체 형식 이용하기
-  [key in MainCategoryKey]: MainCategoryValueType;
-} & {
-  // 다른 타입이랑 합침
-  total: number;
-};
 
 export default function AssetReport() {
   const sampleList: AssetReportType = sample;
 
+  const date = new Date();
+  const year = String(date.getFullYear());
+  const month = ('0' + (date.getMonth() + 1)).slice(-2);
+  const day = ('0' + date.getDate()).slice(-2);
+  const curStart = year.concat('-').concat(month).concat('-').concat('01');
+  const curEnd = year.concat('-').concat(month).concat('-').concat(day);
+  const [startDate, setStartDate] = useState(curStart);
+  const [endDate, setEndDate] = useState(curEnd);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    // loadData();
+    setOpen(false);
+  };
+
   return (
     <>
-      <br />
+      <Card
+        variant="outlined"
+        style={{
+          padding: 15,
+          width: '100%',
+        }}
+      >
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-evenly"
+          alignItems="center"
+          style={{ width: '100%' }}
+        >
+          <Grid xs={8} sm={8} md={8} container justifyContent="start">
+            <Typography
+              style={{
+                fontWeight: 'bold',
+                textAlign: 'center',
+                width: '100%',
+              }}
+            >
+              {startDate}　~　{endDate}
+            </Typography>
+          </Grid>
+          <Grid xs={4} sm={4} md={4} container justifyContent="end">
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ fontWeight: 'bold' }}
+              onClick={handleOpen}
+            >
+              기간설정
+            </Button>
+          </Grid>
+        </Grid>
+      </Card>
     </>
   );
 }
