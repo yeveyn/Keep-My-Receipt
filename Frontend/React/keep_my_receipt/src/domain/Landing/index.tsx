947366d7120d9724f930ef1d9def02de6634378b
LWY <amethyst270@naver.com>
2022-05-13 10:42:07 +0900

    feat: 참고 landing design

--- Frontend/React/keep_my_receipt/src/domain/Landing/index.tsx
+++ Frontend/React/keep_my_receipt/src/domain/Landing/index.tsx
@@ -1,23 +1,74 @@
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+
+// export default function Landing() {
+//   const navigate = useNavigate();
+//   const onClick = () => {
+//     navigate('/login');
+//   };
+//   return (
+//     <>
+//       <Navigation />
+//       <h1>안녕하세요</h1>
+//       <p>
+//         랜딩페이지랜딩페이지랜딩페이지랜딩페이지랜딩페이지랜딩페이지랜딩페이지
+//         랜딩페이지랜딩페이지 랜딩페이지 랜딩페이지 랜딩페이지 랜딩페이지
+//         랜딩페이지 랜딩페이지랜딩페이지 랜딩페이지 랜딩페이지
+//         랜딩페이지랜딩페이지 랜딩페이지랜딩페이지 랜딩페이지 랜딩페이지
+//         랜딩페이지 랜딩페이지 랜딩페이지 랜딩페이지랜딩페이지
+//         랜딩페이지랜딩페이지
+//       </p>
+//       <button onClick={onClick}>로그인하러 가기</button>
+//     </>
+//   );
+// }
+
+import React, { useEffect, useState } from 'react';
+import Header from './Header';
 import Navigation from '../../header';
-export default function Landing() {
-  const navigate = useNavigate();
-  const onClick = () => {
-    navigate('/login');
+import Banner from './Landing/Banner';
+import LandingFirst from './Landing/Landing-1';
+
+import LandingSecond from './Landing/Landing-2';
+
+import LandingThird from './Landing/Landing-3';
+
+import LandingFourth from './Landing/Landing-4';
+
+function Landing() {
+  const [isScroll, setIsScroll] = useState(false);
+
+  const onScrollEvent = () => {
+    if (window.pageYOffset > 0) {
+      setIsScroll(true);
+    }
+    if (window.pageYOffset === 0) {
+      setIsScroll(false);
+    }
   };
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    window.addEventListener('mousewheel', onScrollEvent);
+    return () => {
+      // TODO: 메모리 누수 방지
+      window.removeEventListener('mousewheel', onScrollEvent);
+    };
+  }, []);
   return (
-    <>
+    <div>
       <Navigation />
-      <h1>안녕하세요</h1>
-      <p>
-        랜딩페이지랜딩페이지랜딩페이지랜딩페이지랜딩페이지랜딩페이지랜딩페이지
-        랜딩페이지랜딩페이지 랜딩페이지 랜딩페이지 랜딩페이지 랜딩페이지
-        랜딩페이지 랜딩페이지랜딩페이지 랜딩페이지 랜딩페이지
-        랜딩페이지랜딩페이지 랜딩페이지랜딩페이지 랜딩페이지 랜딩페이지
-        랜딩페이지 랜딩페이지 랜딩페이지 랜딩페이지랜딩페이지
-        랜딩페이지랜딩페이지
-      </p>
-      <button onClick={onClick}>로그인하러 가기</button>
-    </>
+      {/* <Header
+        isScrollBackground={isScroll}
+        isScrollShadow={isScroll}
+        isScrollTransition={isScroll}
+      /> */}
+      <Banner />
+      <LandingFirst />
+      <LandingSecond />
+      <LandingThird />
+      <LandingFourth />
+    </div>
   );
 }
+
+export default Landing;
