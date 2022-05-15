b2f3b87bb33ba0f7ead3ab34c89167daf27820a9
Inho <adiens916@naver.com>
2022-05-16 01:42:19 +0900

    S06P31D104-389 / feat: 태그 CRUD 기능

---
+++ Frontend/React/keep_my_receipt/src/domain/Book/types.ts
@@ -0,0 +1,16 @@
+export type TagType = {
+  tagId: number;
+  tagName: string;
+  parentTag: string | null;
+};
+
+export type AssetType = {
+  ascId: number;
+  ascName: string;
+  balance: number;
+};
+
+export type BudgetType = {
+  bscId: number;
+  bscName: string;
+};
