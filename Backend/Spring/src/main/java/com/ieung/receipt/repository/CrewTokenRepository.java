325a093e85eaaeb25c55dca444fba909ec651c2f
양아름 <yeveyn@gmail.com>
2022-05-16 15:18:05 +0900

    S06P31D104-420 / fix: fcm 알림 전송 실패시 토큰 삭제

--- Backend/Spring/src/main/java/com/ieung/receipt/repository/CrewTokenRepository.java
+++ Backend/Spring/src/main/java/com/ieung/receipt/repository/CrewTokenRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CrewTokenRepository extends JpaRepository<CrewToken, Long>, CrewTokenRepoCommon {
+    void deleteCrewTokensByFcmToken(String fcmToken);
 }
