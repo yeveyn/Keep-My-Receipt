6f41adc04bf53694db29de7d80f123fd17bcf45d
LWY <amethyst270@naver.com>
2022-05-17 00:19:53 +0900

    feat: 알림 toggle

--- Frontend/React/keep_my_receipt/src/domain/Setting/Index/ListItem/AlarmToggle/index.tsx
+++ Frontend/React/keep_my_receipt/src/domain/Setting/Index/ListItem/AlarmToggle/index.tsx
@@ -4,12 +4,38 @@ import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 import ToggleOnIcon from '@mui/icons-material/ToggleOn';
 import ToggleOffIcon from '@mui/icons-material/ToggleOff';
 import { Content } from '../../../../Landing/Landing/Banner/styles';
+import axios from 'axios';
 
 export default function AlarmToggle() {
   const [onAlarm, SetOnAlarm] = useState(true);
   const onClick = () => {
     SetOnAlarm(!onAlarm);
+    // fcm token 가져오기
+    const token = sessionStorage.getItem('fcmToken');
+    //
+    if (onAlarm != true) {
+      axios
+        .put('/api/spring/crew/token/push/allow', { fcmToken: token })
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      axios
+        .put('/api/spring/crew/token/push/disallow', {
+          fcmToken: token,
+        })
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   };
+
   return (
     <Stack alignItems="center">
       <Stack className="board">
