a3266bd673d7f6246f043c729d7757302e9614a2
ssy0061 <dbr00061@naver.com>
2022-05-18 08:58:52 +0900

    S06P31D104-407 / feat: detail props

--- Frontend/React/keep_my_receipt/src/domain/Book/Index/List/index.tsx
+++ Frontend/React/keep_my_receipt/src/domain/Book/Index/List/index.tsx
@@ -1,14 +1,25 @@
 import { Box, Divider, Stack, Typography, Container } from '@mui/material';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import toCurrency from '../../../../services/toCurrency';
 import ListItem from '../ListItem';
 import sample from './sample.json';
 
 export default function BookList({ list }: { list: any }) {
+  const navigate = useNavigate();
+  const onClick = (item: any) => {
+    console.log('transactionId: ' + item.transactionId);
+    console.log('transactionDetailId: ' + item.transactionDetailId);
+  };
   return (
     <>
       {list.map((item: any) => (
-        <Box key={item.transactionDetailId} marginY={1} paddingX="1rem">
+        <Box
+          key={item.transactionDetailId}
+          marginY={1}
+          paddingX="1rem"
+          onClick={() => onClick(item)}
+        >
           <Stack
             direction="row"
             justifyContent="space-between"
