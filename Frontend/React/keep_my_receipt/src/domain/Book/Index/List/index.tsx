654ca240f76827a48174e3b5cd858caf6a78b45e
ssy0061 <dbr00061@naver.com>
2022-05-17 23:04:40 +0900

    S06P31D104-298 / 거래내역 조회 api 확인

--- Frontend/React/keep_my_receipt/src/domain/Book/Index/List/index.tsx
+++ Frontend/React/keep_my_receipt/src/domain/Book/Index/List/index.tsx
@@ -5,44 +5,25 @@ import ListItem from '../ListItem';
 import sample from './sample.json';
 
 export default function BookList({ list }: { list: any }) {
-  // 샘플 파일 불러오기
-  const sampleList = sample;
-
   return (
     <>
       {list.map((item: any) => (
-        <Stack
-          key={item.transactionDetailId}
-          direction="row"
-          justifyContent="space-between"
-        >
-          <Typography>{item.date}</Typography>
-          <Typography>{item.name}</Typography>
-          <Typography>{item.price}</Typography>
-        </Stack>
-      ))}
-
-      <p>sample</p>
-      {sampleList.map((sample) => (
-        <Box key={sample.date}>
+        <Box marginY={1}>
           <Stack
+            key={item.transactionDetailId}
             direction="row"
             justifyContent="space-between"
-            marginTop={3}
+            alignItems="center"
             marginBottom={1}
+            paddingX={1}
           >
-            {/* 왼쪽 끝에 날짜 출력 */}
-            <Typography>{sample.date.slice(8) + '일'}</Typography>
-            {/* 오른쪽 끝에 수입과 지출 */}
-            <Typography>
-              {sample.earn ? '+' + toCurrency(sample.earn) : ''}
-              <br />
-              {toCurrency(sample.cost)}
-            </Typography>
+            <Stack spacing={1}>
+              <Typography variant="body2">{item.date}</Typography>
+              <Typography>{item.name}</Typography>
+            </Stack>
+            <Typography>{item.price}</Typography>
           </Stack>
           <Divider />
-          {/* 해당 날짜의 모든 거래 내역 출력 */}
-          <ListItem items={sample.items} />
         </Box>
       ))}
     </>
--- Frontend/React/keep_my_receipt/src/domain/Book/Index/index.tsx
+++ Frontend/React/keep_my_receipt/src/domain/Book/Index/index.tsx
@@ -1,34 +1,10 @@
-import { Container } from '@mui/material';
+import { Container, Box, Grid } from '@mui/material';
 import IndexHeader from './Header';
 import IndexList from './List';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-
-interface ListTypes {
-  date: any;
-  largeTag: string;
-  name: string;
-  price: number;
-  smallTag?: string;
-  transactionDetailId: number;
-  transactionId: number;
-}
-
-interface ResultTypes {
-  list: ListTypes[];
-  numberOfElements: number;
-  pageNumber: number;
-  size: number;
-  totalElements: number;
-  totalPages: number;
-}
-
-interface ResponseTypes {
-  expenditure: number;
-  income: number;
-  result: ResultTypes;
-}
+import * as qs from 'qs';
 
 export default function BookIndex() {
   // clubId
@@ -50,42 +26,58 @@ export default function BookIndex() {
     },
   });
   const { expenditure, income, result } = res;
+  // 현재 잔액
+  const [balance, setBalance] = useState();
 
+  const HistoryAxios = axios.create({
+    paramsSerializer: (params) =>
+      qs.stringify(params, { arrayFormat: 'repeat' }),
+  });
   // 수입, 지출, 거래내역 가져오기
   const getHistory = async (page?: number) => {
-    await axios
-      .get(`/api/spring/club/${id}/transactions`, {
-        params: {
-          // clubId: id,
-          start: `${target.getFullYear()}-${
-            target.getMonth() + 1 > 9
-              ? target.getMonth() + 1
-              : '0' + (target.getMonth() + 1)
-          }-01`,
-          end: `${targetEnd.getFullYear()}-${
-            targetEnd.getMonth() + 1 > 9
-              ? targetEnd.getMonth() + 1
-              : '0' + (targetEnd.getMonth() + 1)
-          }-${targetEnd.getDate()}`,
-          page: page ? page : 0,
-          size: 5,
-          sort: 'pay_date,DESC',
-        },
-      })
+    await HistoryAxios.get(`/api/spring/club/${id}/transactions`, {
+      params: {
+        // clubId: id,
+        start: `${target.getFullYear()}-${
+          target.getMonth() + 1 > 9
+            ? target.getMonth() + 1
+            : '0' + (target.getMonth() + 1)
+        }-01`,
+        end: `${targetEnd.getFullYear()}-${
+          targetEnd.getMonth() + 1 > 9
+            ? targetEnd.getMonth() + 1
+            : '0' + (targetEnd.getMonth() + 1)
+        }-${
+          today.getMonth() === targetEnd.getMonth()
+            ? today.getDate()
+            : targetEnd.getDate()
+        }`,
+        page: page ? page : 0,
+        size: 5,
+        sort: ['pay_date,DESC', 'id,DESC'],
+      },
+    })
       .then((res) => {
         setRes(res.data.data);
-        console.log(res.data.data);
+        // console.log(res.data.data);
       })
       .catch((e) => {
         console.log(e);
       });
   };
-  // test
-  const febDay = new Date(year, 2, 0);
-  const day29 = new Date(2020, 2, 0);
-  const start = new Date(year, month, 1);
-  const end = new Date(year, month + 1, 0);
-  // test
+  // 현재 예산 잔액 가져오기
+  const getBalance = async () => {
+    await axios
+      .get(`/api/spring/ascategory/${id}/현금 및 현금성자산`)
+      .then((res) => {
+        // console.log(res.data.data[0].balance);
+        setBalance(res.data.data[0].balance);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   useEffect(() => {
     if (
       today.getFullYear() === target.getFullYear() &&
@@ -95,29 +87,30 @@ export default function BookIndex() {
     } else {
       setChecked(false);
     }
-    // console.log(target.toLocaleDateString());
   }, [target]);
 
   useEffect(() => {
+    getBalance();
     getHistory();
   }, [month]);
-
-  useEffect(() => {
-    getHistory();
-  }, []);
   return (
-    <Container maxWidth="md">
-      {/* 월, 지출, 수입 */}
-      <IndexHeader
-        month={month}
-        setMonth={setMonth}
-        target={target}
-        expenditure={expenditure}
-        income={income}
-        checked={checked}
-      />
-      {/* 거래내역 */}
-      <IndexList list={result ? result.list : ''} />
+    <Container maxWidth="md" sx={{ paddingY: 0, paddingX: '1rem' }}>
+      <Grid container direction="column" sx={{ marginBottom: 1 }}>
+        {/* 월, 지출, 수입 */}
+        <IndexHeader
+          month={month}
+          setMonth={setMonth}
+          target={target}
+          expenditure={expenditure}
+          income={income}
+          checked={checked}
+          balance={balance}
+        />
+        {/* 거래내역 */}
+        <Box>
+          <IndexList list={result ? result.list : ''} />
+        </Box>
+      </Grid>
     </Container>
   );
 }
