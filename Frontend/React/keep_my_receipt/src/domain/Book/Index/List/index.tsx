e1de80ac3015159309ef1afa9d7faf89ad0425fb
ssy0061 <dbr00061@naver.com>
2022-05-18 16:19:19 +0900

    S06P31D104-407 / feat: 상세 페이지 연결

--- Frontend/React/keep_my_receipt/src/domain/Book/Index/List/index.tsx
+++ Frontend/React/keep_my_receipt/src/domain/Book/Index/List/index.tsx
@@ -3,11 +3,16 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import toCurrency from '../../../../services/toCurrency';
 import ListItem from '../ListItem';
-import sample from './sample.json';
 
 export default function BookList({ list }: { list: any }) {
   const navigate = useNavigate();
   const onClick = (item: any) => {
+    navigate(`./detail`, {
+      state: {
+        transactionId: item.transactionId,
+        transactionDetailId: item.transactionDetailId,
+      },
+    });
     console.log('transactionId: ' + item.transactionId);
     console.log('transactionDetailId: ' + item.transactionDetailId);
   };
@@ -26,12 +31,15 @@ export default function BookList({ list }: { list: any }) {
             alignItems="center"
             marginBottom={1}
             paddingX={0.5}
+            sx={{
+              '&:hover': { backgroundColor: '#f5f5f5', cursor: 'pointer' },
+            }}
           >
             <Stack spacing={0.5} paddingTop={0.5}>
               <Typography variant="body2">{item.date}</Typography>
               <Typography>{item.name}</Typography>
             </Stack>
-            <Typography>{item.price}</Typography>
+            <Typography>{toCurrency(item.price)}</Typography>
           </Stack>
           <Divider />
         </Box>
