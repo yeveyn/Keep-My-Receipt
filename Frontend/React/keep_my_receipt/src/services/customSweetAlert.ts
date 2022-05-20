7a3e6a5c995311419a2387d6ce5197ef95ac87a2
Inho <adiens916@naver.com>
2022-05-20 13:06:18 +0900

    S06P31D104-500 / feat: 거래 등록, 수정, 삭제 알림창 변경

--- Frontend/React/keep_my_receipt/src/services/customSweetAlert.ts
+++ Frontend/React/keep_my_receipt/src/services/customSweetAlert.ts
@@ -12,3 +12,15 @@ export const WarningToast = Swal.mixin({
     toast.addEventListener('mouseleave', Swal.resumeTimer);
   },
 });
+
+export const ConfirmSwal = (title: string) =>
+  Swal.fire({
+    title: title,
+    icon: 'warning',
+    showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+    confirmButtonColor: '#3085d6', // confrim 버튼 색깔 지정
+    cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
+    confirmButtonText: '확인', // confirm 버튼 텍스트 지정
+    cancelButtonText: '취소', // cancel 버튼 텍스트 지정
+    reverseButtons: false, // 버튼 순서 거꾸로
+  });
