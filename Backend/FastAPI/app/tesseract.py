428327a4bc1d60898e1e4ca7b6512d44f54808cc
seojiwon0702 <seojiwon335@gmail.com>
2022-05-24 00:45:25 +0900

    S06P31D104 / refactor: 주석 추가

--- Backend/FastAPI/app/tesseract.py
+++ Backend/FastAPI/app/tesseract.py
@@ -12,10 +12,13 @@ def findContour(image):
     org_image = image.copy()
     image = imutils.resize(image, height=500)
 
+    # text를 읽어 오기 위해 이미지 색 보정
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    # 윤곽을 잡기 위해 선을 찾아 위치값 추출
     edged = cv2.Canny(gray, 0, 40)
 
+    # 추출한 이미지 윤곽선 중 연결된 사각형 찾기
     cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
@@ -30,9 +33,11 @@ def findContour(image):
             screenCnt = approx
             break
 
+    # 영수증 윤곽을 찾지 못할 경우 원본 이미지 반환
     if screenCnt is None:
         return org_image
 
+    # 윤곽을 찾은 경우 찾은 윤곽선을 기준으로 영수증 수정
     cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
     transform_image = four_point_transform(org_image, screenCnt.reshape(4, 2)*ratio)
 
@@ -40,8 +45,12 @@ def findContour(image):
 
 def tesseractOCR(receipt, type):
     image = plt.imread(receipt)
+    # 사진 영수증의 경우 윤곽선을 찾기 위해 findContour 함수 실행
     if type == "pic":
         image = findContour(image)
+    # psm : 이미지에서 text를 읽는 방식 지정
+    # oem : text를 읽기 위한 ocr 엔진 지정
+    # lang : 추출할 언어 지정
     configs = r'--oem 3 --psm 6'
     text = pytesseract.image_to_string(image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB), config=configs, lang='kor+eng')
     text_list = text.split("\n")
@@ -50,6 +59,7 @@ def tesseractOCR(receipt, type):
     totalPrice = ""
     dealDate = ""
 
+    # 날짜와 거래 금액을 가져오기 위한 검색
     for text in text_list:
         if len(totalPrice)>0 and len(dealDate)>0 : break
         replaceText = text.replace(" ","")
--- Backend/FastAPI/app/uploadFile.py
+++ Backend/FastAPI/app/uploadFile.py
@@ -7,6 +7,7 @@ IMG_DIR = os.path.join(BASE_DIR, 'images/')
 SERVER_IMG_DIR = os.path.join('https://localhost/fast', 'images/')
 
 def uploadImg(image):
+    # 이미지 업로드시 이름 중복 방지를 위해 업로드 시간과 16진수값으로 이름 지정
     currentTime = datetime.datetime.now().strftime("%Y%m%d&H%M%S")
     saved_file_name = ''.join([currentTime,secrets.token_hex(16),".jpg"])
     file_location = os.path.join(IMG_DIR,saved_file_name)
